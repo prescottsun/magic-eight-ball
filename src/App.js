65c2ee69d38d8e138466d0dd5dc00ae8bc68f4e7
Prescott Sun <sunprescott@gmail.com>
2019-12-03 11:08:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import QuestionForm from "./components/questionForm";
+import HistoryLog from "./components/historyLog";
 
 import "./App.css";
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <QuestionForm />
+      <HistoryLog />
     </div>
   );
 }
---
+++ src/components/historyLog.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function HistoryLog() {
+  return <div></div>;
+}
+
+export default HistoryLog;
